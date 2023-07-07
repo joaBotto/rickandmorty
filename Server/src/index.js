681aa42d4657781541a7ba164c4c4032a0023dd2
joaBotto <joaquinbotto2@hotmail.com>
2023-07-07 19:18:09 -0300

    New web server

--- Server/src/index.js
+++ Server/src/index.js
@@ -4,35 +4,24 @@ const data = require("./Utils/data");
 const PORT = 3001;
 const server = http.createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-
-  //Aqui obtenemos la url y el metodo para la solicitud ->
-  const { pathname } = url.parse(req.url);
-  const method = req.method;
-
-  //Ahora verificaremos si la url incluye "/rickandmorty/character" ->
-  if (pathname.startsWith("/rickandmorty/character")) {
-    //Entonces traemos el ID ->
-    const id = pathname.split("/").pop();
-
-    //Buscamos el personaje correspondiente en el archivo data.js ->
-    const character = data.find((character) => character.id === Number(id));
-
-    //Luego verificaremos si se encontro el personaje ->
-    if (character) {
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify(character));
+  console.log(req);
+  const { url } = req;
+  if (url.includes("/rickandmorty/character")) {
+    const id = Number(url.split("/").at(-1));
+    const personaje = data.find((personaje) => personaje.id === id);
+    if (personaje) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(personaje));
     } else {
-      //Si no encontro el presonaje ->
-      res.statusCode = 404;
-      res.end(JSON.stringify({ error: "Character not founded" }));
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "character not founded" }));
     }
   } else {
-    //Si la url no encuentra la ruta, envia una respuesta de error ->
-    res.statusCode = 404;
-    res.end(JSON.stringify({ error: "Route not founded" }));
+    res.writeHead(403, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "route not founded" }));
   }
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, "localhost", null, () => {
   console.log(`Server raised in port ${PORT}`);
 });
